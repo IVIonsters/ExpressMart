31838548e3d076d8629b1b21de9d5027b5c38d98
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-06-26 18:57:53 -0500

    Update tag-routes.js
    Route Definitions:
    
    Defined post route to allow creation of new tag
    
    Defined put route to allow tag to be selected by id value
    
    defined delete route to delete a tag based on it's id value

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,16 +33,55 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagInfo = await Tag.create({
+      tagName: req.body.tagName,
+    });
+    res.status(200).json(tagInfo)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagInfo = await Tag.update({
+      tagName: req.body.tagName,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  if (!tagInfo) {
+    res.status(404).json({ message: "No Tag Found: Wrong ID"});
+    return;
+  }
+  res.status(200).json(tagInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagInfo = Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagInfo) {
+      res.status(404).json({ message: "No Tag Found: Wrong ID" });
+    }
+    res.status(200).json(tagInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
