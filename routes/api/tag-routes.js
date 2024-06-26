251f75f525caa863b7e962b430768ba0652b0a91
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-06-26 18:34:07 -0500

    Update tag-routes.js
    route definitions:
    
    defined main route to find all tag data from model
    
    defined secondary route to find route by ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,34 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findAll({
+      include: ({ model: Product }),
+    })
+    res.status(200).json(tagInfo)
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagInfo) {
+      res.status(404).json({ message: "No tag found! Wrong ID" });
+      return;
+    }
+    res.status(200).json(tagInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
