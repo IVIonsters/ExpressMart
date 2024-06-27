bdd5e6a287fb5ad937c0bbe7e511b97d77d356c0
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-06-26 22:19:33 -0500

    Products
    defined products model, columns , datatypes and features.
    
    defined: product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,6 +113,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productInfo = Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productInfo) {
+      res.status(404).json({ message: "No Product Found: Wrong ID" });
+    }
+    res.status(200).json(productInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
